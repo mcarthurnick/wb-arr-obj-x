995099d101601578acd2c0ec94dd507fb229c6a0
Nick McArthur <mcarthurnick@gmail.com>
2023-06-29 14:16:24 -0600

    solved problems 1 - 6

--- arrays/practice.js
+++ arrays/practice.js
@@ -17,7 +17,9 @@ var arr = [10, 20, 30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr){
+  return arr[0];
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -30,7 +32,9 @@ var arr = [40, 50, 60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[arr.length - 1];
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -43,7 +47,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family){
+for(let i = 0; i < family.length; i++){
+  alert(family[i]);
+}
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -56,7 +64,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper(letters){
+  for(i = letters.length - 1; i >= 0; i--){
+    alert(letters[i]);
+  }
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -69,7 +81,15 @@ var nums = [1, 2, 3, 6, 22, 98, 45, 23, 22, 12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(num){
+  let evenNums = [];
+  for(let i = 0; i < num.length; i++){
+    if(num[i] % 2 === 0){
+      evenNums.push(num[i]);
+    }
+  }
+  return evenNums
+}
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
@@ -86,7 +106,9 @@ var numbersArray = [1, 2, 34, 54, 55, 34, 32, 11, 19, 17, 54, 66, 13];
   and the second item in the array being the odds array (all the odd values from numbersArray).
 */
 
-//Code Here
+function divider(numbersArray){
+  return [numbersArray.filter(e => e % 2 === 0), numbersArray.filter(e => e % 2 !== 0)]
+}
 
 ////////// PROBLEM 7 //////////
 
